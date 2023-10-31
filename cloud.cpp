a17b5b72fbf1ac3adc9c14e98a5b3028d0390bb6
wirr3003 <107315936+wirr3003@users.noreply.github.com>
2023-10-31 11:12:06 +0500

    cloud

---
+++ cloud.cpp
@@ -0,0 +1,8 @@
+#include "graphics.h"
+
+void cloud() {
+  setfillstyle(SOLID_FILL, WHITE, -1);
+  fillellipse(600, 100, 50, 25);
+  fillellipse(625, 115, 50, 25);
+  fillellipse(585, 120, 50, 25);
+}
